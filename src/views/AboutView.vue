d65ca9fa9fdbbbc9f0db70459f3c7c6ebf6d02c0
Tim Schreiber <tim.schreiber@outlook.com>
2022-07-15 19:54:09 +0200

    added mapbox

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{lat}}
+    {{long}}
+    {{acc}}
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const lat = ref(0)
+const long = ref(0)
+const acc = ref(0)
+
+
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position)
+  lat.value = position.coords.latitude
+  long.value = position.coords.longitude
+  acc.value = position.coords.accuracy
+})
+}
+
+function handlePermission() {
+  navigator.permissions.query({name:'geolocation'}).then(function(result) {
+    if (result.state == 'granted') {
+      report(result.state);
+    } else if (result.state == 'prompt') {
+      report(result.state);
+    } else if (result.state == 'denied') {
+      report(result.state);
+    }
+    result.addEventListener('change', function() {
+      report(result.state);
+    });
+  });
+}
+
+function report(state) {
+  console.log('Permission ' + state);
+}
+
+handlePermission();
+
+</script>
---
+++ src/views/MapView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="map">
+    <div id="map-container">
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Mapbox from "mapbox-gl";
+import { onMounted, ref } from 'vue'
+let map:Mapbox;
+let zoom = ref(11)
+let markerZoom = ref(0)
+
+function generateMap() {
+  Mapbox.accessToken = 'pk.eyJ1Ijoid2Vic2Nyb2xsIiwiYSI6ImNsNW1sYm02ajB1ejczY21ncTBtN2o4NjgifQ.eP6LH-g1PeLwlYbegoxV0A';
+  map = new Mapbox.Map({
+  container: 'map-container', // container ID
+  style: 'mapbox://styles/webscroll/cl5mokzbw000914qh5jjhqprw', // style URL
+  center: [16.40,48.20], // starting position [lng, lat]
+  zoom: zoom.value, // starting zoom
+  projection: 'globe' // display the map as a 3D globe
+  });
+  map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+  });
+  addMarker()
+
+  map.on('zoom', (e:Event) => {
+    zoom.value = map.getZoom()
+    markerZoom.value = (zoom.value-2.5)*0.12 - 1
+    if (markerZoom.value < 0) {
+      markerZoom.value = 0
+    }
+    console.log(markerZoom.value)
+  });
+}
+
+function addMarker() {
+  const position = [16.419600898490284,48.16607455063091]
+  const point = document.createElement('div');
+  point.id = 'marker';
+  point.classList.add('tree-container');
+  point.innerHTML= '<div class="tree">'
+
+  new Mapbox.Marker(point)
+  .setLngLat(position)
+  .addTo(map);
+}
+
+onMounted(() => {
+  let width = document.documentElement.clientWidth
+  if(width < 1200) zoom.value = 10
+  if(width < 500) zoom.value = 9
+  console.log(width)
+  generateMap()
+})
+
+
+
+</script>
+<style lang="scss">
+.map{
+  #map-container{
+    height: 100vh;
+  }
+}
+
+.tree{
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: green;
+  transform: scale(v-bind('markerZoom'));
+}
+</style>
