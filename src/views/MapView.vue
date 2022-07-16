e6d0f64d88af00fa54376fd1619f3d5c1a80f5a9
Tim Schreiber <tim.schreiber@beeproduced.com>
2022-07-16 15:20:48 +0200

    working on the marker

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -10,6 +10,7 @@ import { onMounted, ref } from 'vue'
 let map:Mapbox;
 let zoom = ref(11)
 let markerZoom = ref(0)
+let currentSelection = ref('')
 
 function generateMap() {
   Mapbox.accessToken = 'pk.eyJ1Ijoid2Vic2Nyb2xsIiwiYSI6ImNsNW1sYm02ajB1ejczY21ncTBtN2o4NjgifQ.eP6LH-g1PeLwlYbegoxV0A';
@@ -23,6 +24,7 @@ function generateMap() {
   map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
   });
+
   addMarker()
 
   map.on('zoom', (e:Event) => {
@@ -36,16 +38,25 @@ function generateMap() {
   });
 }
 
+function selectTree(id:string):void {
+  currentSelection.value = id
+  const tree = document.querySelector(`[data-id="tree-#${id}"]`) as HTMLElement
+  tree.classList.add('selected')
+}
+
 function addMarker() {
   const position = [16.40,48.20]
   const point = document.createElement('div');
   point.id = 'marker';
   point.classList.add('tree-container');
-  point.innerHTML= '<div class="tree">'
+  point.innerHTML= `<div class="tree" data-id="tree-#1">`
 
   new Mapbox.Marker(point)
   .setLngLat(position)
   .addTo(map);
+
+  const tree = document.querySelector('[data-id="tree-#1"]') as HTMLElement
+  tree.addEventListener('click', () => {selectTree('1')})
 }
 
 onMounted(() => {
@@ -67,10 +78,28 @@ onMounted(() => {
 }
 
 .tree{
+  position: absolute;
   width: 4px;
   height: 4px;
   border-radius: 2px;
   background-color: green;
   transform: scale(v-bind('markerZoom'));
+  cursor: pointer;
+}
+
+.tree.selected{
+  z-index: 1;
+  &::before{
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 5px;
+    height: 5px;
+    border-radius: 50%;
+    background-color: #000;
+    z-index: 0;
+  }
+  
 }
 </style>
